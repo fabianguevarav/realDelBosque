2010daac0b8a8714f6c0c41c16629c0fe558ca8b
fabianguevarav <fabian.guevarav@outlook.com>
2018-10-17 00:59:36 -0500

    agrega contacto.html

--- main.js
+++ main.js
@@ -25,9 +25,17 @@ $('.btnYear').click(() => {
      $('.anuales').toggle();
      $('.ano2017').hide();
      $('.ano2016').hide();
-})
+});
 
-
-//firebase
-
-const provider = new firebase.auth.FacebookAuthProvider();
+// agrega mapa de google a contacto.html
+// Initialize and add the map
+function initMap() {
+	// The location of Uluru
+	var uluru = {lat: 19.387584, lng: -99.223988};
+	// The map, centered at Uluru
+	var map = new google.maps.Map(
+			document.getElementById('map'), {zoom: 15, center: uluru});
+	// The marker, positioned at Uluru
+	var marker = new google.maps.Marker({position: uluru, map: map});
+}
+console.log('holi');
